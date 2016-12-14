665165d6f22db857f1f610e805d86a9b9d33a0b5
Administrator <Administrator@7A304-0047>
2016-12-14 15:23:33 +0800

    用户注册接口对接

--- src/com/example/helloworld/BootActivity.java
+++ src/com/example/helloworld/BootActivity.java
@@ -41,7 +41,7 @@ public class BootActivity extends Activity {
 		OkHttpClient client = new OkHttpClient();
 		
 		Request request = new Request.Builder()
-				.url("http://172.27.0.56:8080/membercenter/api/hello")
+				.url("http://172.27.0.48:8080/membercenter/api/hello")
 				.method("GET", null)
 				.build();
 		
